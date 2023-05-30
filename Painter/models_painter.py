9a372ce432096a29f8bc44c58e272a4ce0833590
sammy <920375887@qq.com>
2023-05-30 09:10:33 +0800

    add forward loss for ac

--- Painter/models_painter.py
+++ Painter/models_painter.py
@@ -417,6 +417,57 @@ class Painter(nn.Module):
                 out.append(self.norm(x))
         return out
 
+    def forward_encoder_ac(self, imgs, tgts, bool_masked_pos):  # img=[1,3,896,448], tgts=[1,3,896,448]
+        # embed patches, 初始化的embed_dim=1024, 没有通道信息: 将输入图像分割成大小相等的patches, 然后将这些子块嵌入到一个更低维的表示中, 应该是变成一长条patch序列, 每个patch由n维向量表示
+        x = self.patch_embed(imgs)   # [1,56,28,1024] B C H W -> B H W C
+        y = self.patch_embed(tgts)   # [1,56,28,1024] B C H W -> B H W C
+        batch_size, Hp, Wp, _ = x.size()
+        seq_len = Hp * Wp
+
+        mask_token = self.mask_token.expand(batch_size, Hp, Wp, -1)  # [1,1,1,1024] -> [1,56,28,1024] 这里的-1就是初始化时候的embed_dim=1024
+        # replace the masked visual tokens by mask_token
+        w = bool_masked_pos.unsqueeze(-1).type_as(mask_token).reshape(-1, Hp, Wp, 1)  # 用于在指定维度上增加一个大小为1的维度
+        w[:] = 1
+        assert torch.sum(1-w) == 0
+        
+        y = y * (1 - w) + mask_token * w  # 这里是构建了输入的target, mask_token就是初始化的要预测的图像, 即对应的任务的预测结果
+
+        # add pos embed w/o cls token  without/   因为后面要将x和y concat 起来，所以要用segment_token_x/y来区分是输入图像还是目标图像, 这个token不加给CLS token
+        x = x + self.segment_token_x  # 广播机制, 给x的每个patch都加上segment_token_x, 表明这是输入图像
+        y = y + self.segment_token_y
+        if self.pos_embed is not None:  # self.pos_embed = [1,197,1024], 其中197=(224/16)*(224/16)+1[CLS token]
+            x = x + get_abs_pos(  # 使用 get_abs_pos 函数获取绝对位置嵌入并将其添加到输入和目标图像张量上, CLS token不加位置编码
+                self.pos_embed, self.pretrain_use_cls_token, (x.shape[1], x.shape[2])
+            )  # 如果num_positions不等于hw的大小, 则需要resize, 这里的get_abs_pos输出shape=[1,56,28,1024]和x一致
+            y = y + get_abs_pos(
+                self.pos_embed, self.pretrain_use_cls_token, (y.shape[1], y.shape[2])
+            )
+        """
+        %%%%%%%%%%%%%在这之上是在构建输入的x和y数据样式
+        那么应该在这之后进行对抗样本的构建
+        1. 对抗样本不能先norm,因为对抗样本的范围在0-1之间
+            1.1 需要先生成数据形式,然后再进行norm用于后面ViT提取特征
+        2. mask_token, segment_token, position_embed都不能添加扰动, 只能是原始图像上加扰动
+            2.1 那么创建对抗样本的位置需要改变, 不在这里
+        """
+        merge_idx = 2
+        x = torch.cat((x, y), dim=0)  # 按batch_size维度拼接, [2,56,28,1024]
+        x = x.reshape(x.shape[0], 2, Hp//2, Wp, x.shape[-1]) #[col=2, row=2, h=28, w=28, C=1024]
+        x = x.flatten(0,1)
+        
+        # apply Transformer blocks
+        out = [] 
+        for idx, blk in enumerate(self.blocks):  # 这里是用ViT做encoder, 24 blocks for ViT-large
+            x = blk(x)  # 输出的shape没有改变 [4,28,28,1024]
+            if idx == merge_idx:  # merge the early features of the input image and the output image, 3 blocks, 所以之后变成1的batch size了?
+                x = (x[:x.shape[0]//2] + x[x.shape[0]//2:]) * 0.5  # (row=2, 28, 28, 1024)
+            if idx in [5, 11, 17, 23]:
+                out.append(self.norm(x).split([1, 1], dim=0))
+        out_a = torch.cat([_[0] for _ in out])
+        out_c = torch.cat([_[1] for _ in out])
+        return out_a, out_c  # idx=5,11,17,23时, 保留这几层的输出
+
+
     def forward_decoder(self, x):
         # predictor projection
         x = torch.cat(x, dim=-1)
@@ -430,13 +481,16 @@ class Painter(nn.Module):
         x = self.decoder_pred(x) # Bx3xHxW
         return x
 
-    def forward_loss(self, pred, tgts, mask, valid):
+    def forward_loss(self, pred, tgts, mask, valid, ignore_D_loss=False):
         """
         tgts: [N, 3, H, W]
         pred: [N, 3, H, W]
         mask: [N, L], 0 is keep, 1 is remove, 
         valid: [N, 3, H, W]
         """
+        if ignore_D_loss: # wt
+            mask[:, mask.shape[1]//2:] = False
+            
         mask = mask[:, :, None].repeat(1, 1, self.patch_size**2 * 3)
         mask = self.unpatchify(mask)
 
@@ -447,7 +501,7 @@ class Painter(nn.Module):
         if inds_ign.sum() > 0:
             valid[inds_ign] = 0.
 
-        mask = mask * valid
+        mask = mask * valid   ## [1,3,896,448]  mask[:,:,:448,:]均为0, mask[:,:,448:,:]均为1, valid全为1
 
         target = tgts
         if self.loss_func == "l1l2":
